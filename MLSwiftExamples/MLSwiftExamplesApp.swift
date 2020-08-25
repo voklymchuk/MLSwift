8b5b9e8dbc83dac48bfabf8bc669f43f8d3f061f
Valerii Klymchuk <voklymchuk@gmail.com>
2020-08-25 07:11:25 -0400

    Initial project setup

---
+++ MLSwiftExamples/MLSwiftExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLSwiftExamplesApp.swift
+//  MLSwiftExamples
+//
+//  Created by Valerii Klymchuk on 8/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MLSwiftExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
