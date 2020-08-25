c85d502ac98c70cd3d7d3756656f6004f5c9383c
Valerii Klymchuk <voklymchuk@gmail.com>
2020-08-25 06:54:02 -0400

    Initial Commit

---
+++ MLSwift/MLSwift.h
@@ -0,0 +1,18 @@
+//
+//  MLSwift.h
+//  MLSwift
+//
+//  Created by Valerii Klymchuk on 8/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLSwift.
+FOUNDATION_EXPORT double MLSwiftVersionNumber;
+
+//! Project version string for MLSwift.
+FOUNDATION_EXPORT const unsigned char MLSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLSwift/PublicHeader.h>
+
+
